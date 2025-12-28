37b0649d8cba285605a1abb7a91b0e072e9b3da0
Rudresh Gowda <rudresh9nr@gmail.com>
2025-12-28 21:31:15 +0530

    add vercel configuration

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-2)jm+50*0$)&v@*xyuw65^8y7kdish4#kw6h!m(!5&fp9^-n$k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
